6c901e71ddc236e278fd40339a1a6a9a1aa12bb8
wawilson810 <wawilson810@gmail.com>
2022-04-11 15:04:15 -0400

    Both scripts complete except txt file export

---
+++ PyPoll/main.py
@@ -0,0 +1,36 @@
+import os
+import csv
+
+path = "C:\\Users\\wawil\\python-challenge\\PyPoll\\"
+poll_csv = os.path.join(path, 'Resources','election_data.csv')
+
+def line():
+    print("--------------------")
+
+with open(poll_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    candidates = []
+    total = 0
+    votes = {}
+    csv_header = next(csvreader)
+    for row in csvreader:
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            votes[row[2]] = 1
+        else:
+            votes[row[2]] += 1
+        total+=1
+    
+    print("Election Results")
+    line()
+    print(f"Total Votes: {total}")
+    line()
+
+    for keys in votes:
+        print(f"{keys}: {round( 100*(votes[keys]/total),3)}% ({votes[keys]})")
+        if votes[keys] == max(votes.values()):
+            winner = keys
+    
+    line()
+    print(f"Winner: {winner}")
+    line()
