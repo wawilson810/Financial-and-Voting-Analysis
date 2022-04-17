f62257dae7ef6825f053d3399d7f3d7152fd9c89
wawilson810 <wawilson810@gmail.com>
2022-04-16 20:39:59 -0400

    Commented the scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,47 +1,61 @@
-from calendar import month
 import os
 import csv
+
+#Writing the paths for reading in the csv file as well as writing the txt file with the results respectively
 
 path = "C:\\Users\\wawil\\python-challenge\\PyBank\\"
 budget_csv = os.path.join(path, 'Resources','budget_data.csv')
 writepath = path + "analysis\\results.txt"
 
+#Opening the csv file to read in the data
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    #Storing the header row and moving past it to get to the data
     csv_header = next(csvreader)
 
+    #Initializing lists to store the months and profits as well as the net profit as 0
     months = []
     net_prof = 0
     prof = []
 
+    #Reading in each row of data
     for row in csvreader:
+        #Adding each month to the proper list as well as adding the profit from that month to the total
         months.append(row[0])
         net_prof+= float(row[1])
+
+        #Testing if we are reading in the first row. If true, we store the profit as the previous month's profit.
+        #If it is a row after the first, we subtract the previous month's profit from the current month's to get the change and then store that month's as the previous for the next loop
         if len(months) == 1:
             prev_prof = float(row[1])
         else:
             prof.append(float(row[1])-prev_prof)
             prev_prof = float(row[1])
 
+    #Calculating the average change in profit, the maximum change and the minimum.
     avg_prof = sum(prof)/len(prof)
     max_prof = max(prof)
     min_prof = min(prof)
 
+    #Looping through the list of changes in profit to determine the index of the maximum and minimum to store the values to display which month they occured in
     for i in range(len(prof)):
         if prof[i] == max_prof:
             max_ind = i+1
         elif prof[i] == min_prof:
             min_ind = i+1
 
+    #Printing the results to the terminal
     print("Financial Analysis")
     print("--------------------------")
     print(f"Total Months: {len(months)}")
     print(f"Total: ${net_prof}")
+    #Formatting the changes in profit in a way that matches the desired output
     print(f"Average Change: ${round(avg_prof, 2)}")
     print(f"Greatest Increase in Profits: {months[max_ind]} (${int(max_prof)})")
     print(f"Greatest Decrease in Profits: {months[min_ind]} (${int(min_prof)})")
 
+#Exporting what was displayed in the terminal to a txt file in the analysis folder
 with open(writepath, "w") as file:
     lines = ["Financial Analysis", "--------------------------", f"Total Months: {len(months)}", f"Total: ${net_prof}",
     f"Average Change: ${round(avg_prof, 2)}", f"Greatest Increase in Profits: {months[max_ind]} (${int(max_prof)})",
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,51 @@
 import os
 import csv
 
+#Writing the paths for reading in the csv file as well as writing the txt file with the results respectively
 path = "C:\\Users\\wawil\\python-challenge\\PyPoll\\"
 writepath = path + "analysis\\results.txt"
 poll_csv = os.path.join(path, 'Resources','election_data.csv')
 
+#A simple function to display a line of dashes to be used when displaying the results
 def line():
     print("--------------------")
 
+#Opening the csv file to read in the data
 with open(poll_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    #Initializing a list of for the unique candidates, the total votes as 0 and a dictionary with the candidate as the key and the votes they received as the value
     candidates = []
     total = 0
     votes = {}
+    
+    #Storing the header row and moving past it to get to the data
     csv_header = next(csvreader)
+
+    #Reading through the data row by row
     for row in csvreader:
+
+        #Testing if the candidate in each row is already in the list of candidates. If they are not, they are added to the list.
+        #Additionally, they are added to the dictionary as a key with the initial value of 1 vote.
+        #If the candidate is already on the list, a vote is added to their total
         if row[2] not in candidates:
             candidates.append(row[2])
             votes[row[2]] = 1
         else:
             votes[row[2]] += 1
+        #Counting the total votes
         total+=1
     
+    #Displaying the results of the election in the terminal
     print("Election Results")
     line()
     print(f"Total Votes: {total}")
     line()
 
+    #Looping through the candidates and displaying them, the percentage of the vote they received as well as the total votes they received
     for keys in votes:
         print(f"{keys}: {round( 100*(votes[keys]/total),3)}% ({votes[keys]})")
+        #Testing if each candidate received the maximum number of votes to determine the winner
         if votes[keys] == max(votes.values()):
             winner = keys
     
@@ -36,6 +53,7 @@ with open(poll_csv) as csvfile:
     print(f"Winner: {winner}")
     line()
 
+#Exporting what was displayed in the terminal to a txt file in the analysis folder
 with open(writepath, "w") as file:
     file.write('\n'.join(["Election Results", "--------------------------", 
     f"Total Votes: {total}"]))
